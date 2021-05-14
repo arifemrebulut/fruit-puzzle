b6e242b3de8e319ccfbcf885bbcb8eeb5f42f2bb
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-05-14 14:37:51 +0300

    Create PlayerInput for swipe detection

--- FruitPuzzle/Assets/Scripts/FruitMovement.cs
+++ FruitPuzzle/Assets/Scripts/FruitMovement.cs
@@ -5,11 +5,25 @@ public class FruitMovement : MonoBehaviour
 {
     [SerializeField] float gridMovementDuration;
 
+    private PlayerInput playerInput;
+
     private bool isGridMoving;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
 
     void Update()
     {
-
+        if (playerInput.swipeDirection == Vector3.up)
+        {
+            StartCoroutine(GridMovement(Vector3.up));
+        }
+        else if (playerInput.swipeDirection == Vector3.down)
+        {
+            StartCoroutine(GridMovement(Vector3.down));
+        }
     }
 
     private IEnumerator GridMovement(Vector3 direction)
