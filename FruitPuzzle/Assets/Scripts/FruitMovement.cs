4b3be7d9e8e80fbcff6661fec3d4a5e69c46d6da
Emre Bulut <80202387+arifemrebulut@users.noreply.github.com>
2021-05-14 13:45:45 +0300

    Create grid based movement

--- FruitPuzzle/Assets/Scripts/FruitMovement.cs
+++ FruitPuzzle/Assets/Scripts/FruitMovement.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float gridMovementDuration;
 
-    // Update is called once per frame
+    private bool isGridMoving;
+
     void Update()
     {
-        
+
+    }
+
+    private IEnumerator GridMovement(Vector3 direction)
+    {
+        isGridMoving = true;
+
+        float elapsedTime = 0;
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + direction;
+
+        while (elapsedTime < gridMovementDuration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, (elapsedTime / gridMovementDuration));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+
+        isGridMoving = false;
     }
 }
